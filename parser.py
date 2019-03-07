d20d2a761d71a04a2f9fe4336f7fe69522c113e6
gmkseta <gmkseta@gmail.com>
2019-03-08 00:30:33 +0900

    handler position

--- parser.py
+++ parser.py
@@ -7,31 +7,28 @@ if __name__ == "__main__":
     try:
         print("init")
         driver = CrawlBrowser()
+        try:
+            driver = CrawlBrowser()
+
+            driver.login()
+
+            driver.get_list()
+
+            for i in driver.data:
+                driver.get_detail(i)
+
+            my_dict = driver.data
+
+            with open('total.csv', 'w', encoding='utf-8') as f:  # Just use 'w' mode in 3.x
+                w = csv.DictWriter(f, my_dict[0].keys())
+                w.writeheader()
+                for key, value in my_dict.items():
+                    w.writerow(value)
+        except:
+            driver.browser.close()
+            print("error")
+        finally:
+            driver.browser.close()
+            print("end")
     except:
         print("init error")
-
-    try:
-        driver = CrawlBrowser()
-
-        driver.login()
-
-        driver.get_list()
-
-        for i in driver.data:
-            driver.get_detail(i)
-
-        my_dict = driver.data
-
-        with open('total.csv', 'w', encoding='utf-8') as f:  # Just use 'w' mode in 3.x
-            w = csv.DictWriter(f, my_dict[0].keys())
-            w.writeheader()
-            for key, value in my_dict.items():
-                w.writerow(value)
-
-
-    except:
-        driver.browser.close()
-        print("error")
-    finally:
-        driver.browser.close()
-        print("end")
